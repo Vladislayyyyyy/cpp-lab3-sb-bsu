1e2d2f9f09c379b9b977f709aa335ea9354e3d60
Vlads <87931410+Vladislayyyyyy@users.noreply.github.com>
2023-10-24 19:52:19 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main (){
     int k,i = 1, sum_k = 0, number;
     cout << "Enter the amount of integers to sum = "; cin >> k;
+    if (k == 0) cout << "No" << endl;
+    else
     do {
         cout << "Enter integer nr. " << i << ": ";
         cin >> number;
@@ -11,4 +13,3 @@ int main (){
     cout << "The total sum of " << k << "integer is : " << sum_k;
     return 0;
 }
-
